005a449d7a4794a750049e0883ec6af05e3839a5
Kacper Kałużny <kacper.kaluzny7@gmail.com>
2026-02-25 19:56:57 +0100

    Poprawa konfiguracji

--- MobileApp/src/components/ui.tsx
+++ MobileApp/src/components/ui.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import { ActivityIndicator, Pressable, StyleSheet, Text, View, type ViewStyle } from "react-native";
 
 export const colors = {
